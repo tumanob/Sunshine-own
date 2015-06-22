d38d94c8429bce27e657ae452024ecdd33abf9ab
tumanob <tumanob@gmail.com>
2015-06-22 15:43:47 +0600

    initial setup with blank screen problem

---
+++ app/src/androidTest/java/com/tumanob/sunshineown/sunshine_own/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.tumanob.sunshineown.sunshine_own;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
